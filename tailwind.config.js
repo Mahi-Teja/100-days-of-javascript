fcb1051ec786db66e4ca54f091c7c06963598aa4
Mahi-Teja <mahineeli899@gmail.com>
2024-01-22 16:12:35 +0530

    Added packages

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: 
+  ['*.{html,js}','../*.{html,js  }','./src/**/*.css','01/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
