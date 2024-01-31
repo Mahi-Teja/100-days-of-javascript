f12b3197f17a62640664ff4b56c49d97c7ce8ad9
Mahi-Teja <mahineeli899@gmail.com>
2024-01-31 22:59:07 +0530

    Added Day 10 files

---
+++ 10/imgSlider.js
@@ -0,0 +1,48 @@
+const leftBtn = document.querySelector(".prev-btn");
+const rightBtn = document.querySelector(".next-btn");
+const imgBox = document.querySelector("#img-box")
+const images = document.querySelectorAll(".img");
+
+const sliderBox = document.querySelector(".slider-container")
+const sliderLength = images.length;
+
+
+let count =0;
+
+images.forEach(function(img,index){
+    img.style.left=`${index*100}%`
+})
+
+rightBtn.addEventListener("click",nextSlide);
+
+function nextSlide(){
+    // alert()
+    count++;
+    if(count>sliderLength-1){
+        count=0
+    }
+    let m=`translateX(-${count*100}%)`;
+    images.forEach(function(img){
+        img.style.transform=m;
+       
+    });
+}
+
+leftBtn.addEventListener("click",prevSlide);
+function prevSlide(){
+    count--;
+    if(count<0){
+        count=sliderLength-1
+    }
+    let m=`translateX(-${count*100}%)`;
+    images.forEach(function(img){
+        img.style.transform=m;
+        
+    });
+}
+
+let slideShow =setInterval(autoSlide,3000);
+
+function autoSlide(){
+    nextSlide()
+}
